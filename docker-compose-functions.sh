f139acee2e2dfe45fb0e7cc271f93c57837cfe9a
Hashib Khondokar <hashib@Hashibs-MBP.lan>
2023-04-30 04:23:23 +0900

    fix docker-compose file path

--- docker-compose-functions.sh
+++ docker-compose-functions.sh
@@ -1,11 +1,11 @@
 function psql_start {
-    docker-compose -f docker-compose-psql.yml up -d
+    docker-compose -f /Users/hashib/Codes/DockerComposeFiles/docker-compose-psql.yml up -d
 }
 
 function psql_stop {
-    docker-compose -f docker-compose-psql.yml down
+    docker-compose -f /Users/hashib/Codes/DockerComposeFiles/docker-compose-psql.yml down
 }
 
 function psql {
-    docker-compose -f docker-compose-psql.yml exec postgres psql -U postgres
+    docker-compose -f /Users/hashib/Codes/DockerComposeFiles/docker-compose-psql.yml exec postgres psql -U postgres
 }
